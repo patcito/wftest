84973e6aae679a80ab493a6da81bb1627a985eb5
Patrick Aljord <patcito@gmail.com>
2021-05-05 19:51:07 +0400

    lejflskfjsf wefrwer fwef

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 let constants = ["test", "shoop"]
-let x = []
-exec("git diff origin/main HEAD", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-    x = "stdout"
-});
+let x = execSync("git diff origin/main HEAD").toString()
 console.log(x)
