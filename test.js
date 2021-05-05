411c82793715134ad014d9e2b7ed2803e839dc5c
Patrick Aljord <patcito@gmail.com>
2021-05-05 20:57:43 +0400

    lejflskfjsf wefrwer fwef

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
 const { execSync } = require("child_process");
-let constants = ["test", "shoop"]
-let x = execSync("git diff origin/main HEAD").toString()
-console.log(x)
+let constants = ["test","shoop"];
+let x = execSync("git diff origin/main HEAD").toString();
+let found = {}
+let currentFile = ""
+x.split("\n").forEach((line) => {
+    if(line.includes("diff --git a")){
+        currentFile = line.split(" b/")[1]
+    }
+  constants.forEach((constant) => {
+    if (line.includes(constant)) {
+      console.log("line includes " + constant, currentFile);
+      found[currentFile] = constant
+    }
+  });
+});
+Object.keys(found).forEach((key)=>{
+    console.log(`Found ${found[key]} in file ${key}`)
+})
+console.log(found)
